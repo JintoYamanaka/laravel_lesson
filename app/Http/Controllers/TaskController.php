895f059f3e6cd358f0c3871600306475ca65217f
JintoYamanaka <yamajin.ips11@gmail.com>
2019-11-19 17:33:20 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Folder;
+use App\Task; // ★ 追加
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+    {
+        $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+            'tasks' => $tasks,
+        ]);
+    }
+}
